1d53a7e79a1292a0064704fc4daaa5649dfe5c6d
Laeyoung Chang <laeyoung@bhaptics.com>
2023-09-20 13:40:40 +0900

    step1

--- app/src/main/java/com/example/mycomposeapplication/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeapplication/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.mycomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mycomposeapplication.ui.theme.MyComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +43,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Row(modifier = Modifier.padding(8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "hello",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(2.0.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column() {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
